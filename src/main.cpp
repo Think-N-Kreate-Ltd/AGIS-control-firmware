8f35b5f61588eec3544a70e31b35f2c866427029
Nhan-Office <vntran@thinknkreate.com>
2023-04-18 16:25:21 +0800

    refactor names and remove unused

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,3 @@
-/*********
-  Rui Santos
-  Complete project details at
-https://RandomNerdTutorials.com/esp32-esp8266-input-data-html-form/
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-*********/
 /*
   wifi ssid:AutoConnectAP, password:password.
   go to http://<IPAddress>/update for OTA update
@@ -47,29 +36,28 @@ buttonState_t buttonState = buttonState_t::IDLE;
 enum droppingState_t {NOT_STARTED, STARTED, STOPPED};
 // Initially, dropping is not started
 droppingState_t droppingState = droppingState_t::NOT_STARTED;
-// volatile bool dropStarted = false; 
 
 // var for checking the time
-volatile bool no_drop_with_20s = false; // true if no drop appears in next 20s
+volatile bool noDropWithin20s = false; // true if no drop appears in next 20s
 volatile bool volume_exceed = false;    // true if numDrops exceed amount
 
 // var for timer0 interrupt
 // for reading the sensor value
 volatile int occur;
 // for measuring the time that sensor detect a drop
-unsigned long start_time = 0;
+volatile unsigned long startTime = 0;
 // for measuring the time that sensor detect a drop is disappear
-unsigned long leave_time = 0;
+volatile unsigned long leaveTime = 0;
 // for measuring the time that sensor detect the next drop
-unsigned long next_time = 0;
-unsigned long totalTime = 0; // for calculating the time used within 15s
-unsigned int numDrops = 0;   // for counting the number of drops within 15s
+volatile unsigned long nextTime = 0;
+volatile unsigned long totalTime = 0; // for calculating the time used within 15s
+volatile unsigned int numDrops = 0;   // for counting the number of drops within 15s
 volatile unsigned int dripRate = 0;       // for calculating the drip rate
 volatile unsigned int time1Drop = 0;      // for storing the time of 1 drop
 volatile unsigned int timeBtw2Drops = UINT_MAX; // i.e. no more drop recently
 volatile float infusedVolume = 0;  // unit: mL
 volatile unsigned long infusedTime = 0;     // unit: seconds
-unsigned long infusionStartTime = 0;
+volatile unsigned long infusionStartTime = 0;
 
 volatile unsigned int dripRateSamplingCount = 0;  // use for drip rate sampling
 volatile unsigned int numDropsInterval = 0;  // number of drops in 15 seconds
@@ -85,8 +73,6 @@ ezButton limitSwitch_Up(37);   // create ezButton object that attach to pin 7;
 ezButton limitSwitch_Down(38); // create ezButton object that attach to pin 7;
 
 // var for checking the currently condition
- // true if it is controlled by the web button currently
-volatile bool web_state = false;
 // state that shows the condition of web button
 int web_but_state = 0; 
 // state that shows the condition of auto control
@@ -101,9 +87,6 @@ volatile bool infusionCompleted = false;   // true when infusion is completed
 volatile bool infusionStarted = false;     // true when button_ENTER is pressed the 1st time
                                          // to activate autoControl()
 
-// volatile bool drippingIsStable = true; // true when receiving the first NUM_DROPS_TILL_STABLE drops
-//                                        // initially needs to set to true, otherwise autoControl() cannot start
-
 volatile bool firstDropDetected = false; // to check when we receive the 1st drop
 volatile bool autoControlOnPeriod = false;
 bool homingCompleted = false;   // true when lower limit switch is activated
@@ -111,7 +94,6 @@ bool homingCompleted = false;   // true when lower limit switch is activated
 // To reduce the sensitive of autoControl()
 // i.e. (targetDripRate +/-5) is good enough
 #define AUTO_CONTROL_ALLOW_RANGE 5
-// #define DRIP_RATE_SAMPLE_PERIOD  5   // 5 seconds
 #define AUTO_CONTROL_ON_TIME     50  // motor will be enabled for this amount of time (unit: ms)
 #define AUTO_CONTROL_TOTAL_TIME  1000  // 1000ms
 #define DROP_DEBOUNCE_TIME       10   // if two pulses are generated within 10ms, it must be detected as 1 drop
@@ -139,12 +121,11 @@ const char *PARAM_AUTO_1 = "auto1";
 
 // Function prototypes
 int check_state();
-void Motor_On_Up();
-void Motor_On_Down();
-void Motor_Off();
-// void Motor_Mode();
-const char *get_motor_state(motorState_t state);
-const char *get_button_state(buttonState_t state);
+void motorOnUp();
+void motorOnDown();
+void motorOff();
+const char *getMotorState(motorState_t state);
+const char *getButtonState(buttonState_t state);
 void initWebSocket();
 void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client,
              AwsEventType type, void *arg, uint8_t *data, size_t len);
@@ -198,8 +179,8 @@ hw_timer_t *Timer2_cfg = NULL; // create a pointer for timer2
 
 // timer0 interrupt, for sensor detected drops and measure the time
 void IRAM_ATTR dropSensor() {
-  static bool occur_state = false; // true when obstacle detected
-  static int time_for_no_drop; // counting when no drop appears, for measuring
+  static bool occurState = false; // true when obstacle detected
+  static int timeWithNoDrop; // counting when no drop appears, for measuring
                                // the time that have no drop
   static unsigned int timeDifference; 
 
@@ -208,10 +189,10 @@ void IRAM_ATTR dropSensor() {
   dripRateSamplingCount++;  // increment 1ms
 
   if (occur == 1) {
-    time_for_no_drop = 0;
-    no_drop_with_20s = false;
+    timeWithNoDrop = 0;
+    noDropWithin20s = false;
     droppingState = droppingState_t::STARTED; // droping has started
-    if (!occur_state) { // condition that check for the drop is just detected
+    if (!occurState) { // condition that check for the drop is just detected
 
 
       // FIRST DROP DETECTION
@@ -225,9 +206,9 @@ void IRAM_ATTR dropSensor() {
 
       numDropsInterval++;
 
-      occur_state = true;
-      next_time = millis();
-      timeDifference = next_time - start_time;
+      occurState = true;
+      nextTime = millis();
+      timeDifference = nextTime - startTime;
       // FALSE COUNT DETECTION
       // if 2 consecutive pulses are within 10ms, it's false alarm
       if (timeDifference > DROP_DEBOUNCE_TIME) {
@@ -235,27 +216,27 @@ void IRAM_ATTR dropSensor() {
         timeBtw2Drops = timeDifference;
       }
       totalTime += timeBtw2Drops;
-      start_time = millis();
+      startTime = millis();
     }
   }
   else if (occur == 0) {
-    time_for_no_drop++;
-    if (occur_state) {
-      leave_time = millis(); // record the time for measuring
-      occur_state = false;
-      time1Drop = leave_time - start_time;
+    timeWithNoDrop++;
+    if (occurState) {
+      leaveTime = millis(); // record the time for measuring
+      occurState = false;
+      time1Drop = leaveTime - startTime;
     }
   }
 
   // call when no drop appears within 20s, reset all data
-  if ((time_for_no_drop >= 20000) && (droppingState == droppingState_t::STARTED)) {
+  if ((timeWithNoDrop >= 20000) && (droppingState == droppingState_t::STARTED)) {
     time1Drop = 0;
     // numDrops = 0;
 
     // TODO: how do we define totalTime? Should it be RTC time or only the time
     // when we have drops?
     totalTime = 0;
-    no_drop_with_20s = true;
+    noDropWithin20s = true;
 
     // set timeBtw2Drops to a very large number
     timeBtw2Drops = UINT_MAX;
@@ -272,18 +253,6 @@ void IRAM_ATTR dropSensor() {
   //   // alert("VolumeExceed");
   // }
 
-  // Calculate dripRate using number of drops in every DRIP_RATE_SAMPLE_PERIOD seconds
-  // if (dripRateSamplingCount == (DRIP_RATE_SAMPLE_PERIOD * 1000)) {
-  //   // 15 seconds reached
-  //   dripRate = numDropsInterval * (60 / DRIP_RATE_SAMPLE_PERIOD);
-  //   // enableAutoControl = true;
-
-  //   // reset for the next sampling
-  //   dripRateSamplingCount = 0;
-  //   numDropsInterval = 0;
-  // }
-
-  // NOTE: use a fixed DRIP_RATE_MAX_RANGE to ignore huge drip rate from unstable measurements
   // get latest value of dripRate
   dripRate = 60000 / timeBtw2Drops; // TODO: explain this formular
 
@@ -291,18 +260,13 @@ void IRAM_ATTR dropSensor() {
   if (!infusionCompleted) {
     infusedTime = (millis() - infusionStartTime) / 1000;  // in seconds
   }
-
-  // NOTE: maybe we should average most recent dripRate,
-  // s.t. the auto control is not too sensitive and motor runs too frequently
 }
 
 void IRAM_ATTR autoControl() { // timer1 interrupt, for auto control motor
   // Only run autoControl() when the following conditions satisfy:
   //   1. button_ENTER is pressed
-  //   2. dripping is stable, i.e. drippingIsStable = true
   //   3. targetDripRate is set on the website by user
   //   4. infusion is not completed, i.e. infusionCompleted = false
-  // TODO: update value of infusionCompleted in other function
 
   autoControlCount++;
   if (firstDropDetected) {
@@ -332,28 +296,28 @@ void IRAM_ATTR autoControl() { // timer1 interrupt, for auto control motor
 
     // if currently SLOWER than set value -> speed up, i.e. move up
     if (dripRate < (targetDripRate - AUTO_CONTROL_ALLOW_RANGE)) {
-      Motor_On_Up();
+      motorOnUp();
     }
 
     // if currently FASTER than set value -> slow down, i.e. move down
     else if (dripRate > (targetDripRate + AUTO_CONTROL_ALLOW_RANGE)) {
-      Motor_On_Down();
+      motorOnDown();
     }
 
     // otherwise, current drip rate is in allowed range -> stop motor
     else {
-      Motor_Off();
+      motorOff();
     }
   }
   else {
-    // Motor_Off();
+    // motorOff();
 
     if (infusionCompleted && !homingCompleted) {
     // homing the roller clamp, i.e. move it down to completely closed position
       homingRollerClamp();
     }
     else {
-      Motor_Off();
+      motorOff();
     }
   }
 
@@ -372,13 +336,13 @@ void IRAM_ATTR motorControl() {
   // Use button_UP to manually move up
   if (!button_UP.getState()) {  // touched
     buttonState = buttonState_t::UP;
-    Motor_On_Up();
+    motorOnUp();
   }
 
   // Use button_DOWN to manually move down
   if (!button_DOWN.getState()) {  // touched
     buttonState = buttonState_t::DOWN;
-    Motor_On_Down();
+    motorOnDown();
   }
 
   // Use button_ENTER to toggle autoControl()
@@ -396,14 +360,13 @@ void IRAM_ATTR motorControl() {
       numDrops = 0;
       infusedVolume = 0.0f;
       infusedTime = 0;
-
       infusionStarted = true;
     }
   }
 
   if (button_UP.isReleased() || button_DOWN.isReleased() || button_ENTER.isReleased()) {
     buttonState = buttonState_t::IDLE;
-    Motor_Off();
+    motorOff();
   }
 }
 
@@ -550,7 +513,7 @@ void loop() {
   // DEBUG:
   // Serial.printf(
   //     "dripRate: %u \ttarget_drip_rate: %u \tmotor_state: %s\tint_time2: %u\n",
-  //     dripRate, targetDripRate, get_motor_state(motorState), timeBtw2Drops);
+  //     dripRate, targetDripRate, getMotorState(motorState), timeBtw2Drops);
 
   // Serial.printf("numDrops: %d, \ttargetNumDrops: %d, \t%d\n", numDrops, targetNumDrops, infusionCompleted);
 }
@@ -571,7 +534,7 @@ int check_state() {
   return state;
 }
 
-void Motor_On_Up() {
+void motorOnUp() {
   limitSwitch_Up.loop();   // MUST call the loop() function first
 
   if (limitSwitch_Up.getState()) { // untouched
@@ -584,11 +547,11 @@ void Motor_On_Up() {
     motorState = motorState_t::UP;
   }
   else { // touched
-    Motor_Off();
+    motorOff();
   }
 }
 
-void Motor_On_Down() {
+void motorOnDown() {
   limitSwitch_Down.loop();   // MUST call the loop() function first
 
   if (limitSwitch_Down.getState()) { // untouched
@@ -601,18 +564,18 @@ void Motor_On_Down() {
     motorState = motorState_t::DOWN;
   }
   else { // touched
-    Motor_Off();
+    motorOff();
   }
 }
 
-void Motor_Off() {
+void motorOff() {
   analogWrite(MOTOR_CTRL_PIN_1, 0);
   analogWrite(MOTOR_CTRL_PIN_2, 0);
 
   motorState = motorState_t::OFF;
 }
 
-const char *get_motor_state(motorState_t state) {
+const char *getMotorState(motorState_t state) {
   switch (state) {
   case motorState_t::UP:
     return "UP";
@@ -626,7 +589,7 @@ const char *get_motor_state(motorState_t state) {
   }
 }
 
-const char *get_button_state(buttonState_t state) {
+const char *getButtonState(buttonState_t state) {
   switch (state) {
   case buttonState_t::UP:
     return "UP";
@@ -742,7 +705,7 @@ void sendDataWs() {
 // TODO: refactor: create a function to send json object as websocket message
 
 // Move down the roller clamp to completely closed position
-// Copied and modified from Motor_On_Down()
+// Copied and modified from motorOnDown()
 void homingRollerClamp() {
   limitSwitch_Down.loop();   // MUST call the loop() function first
 
@@ -756,7 +719,7 @@ void homingRollerClamp() {
     motorState = motorState_t::DOWN;
   }
   else { // touched
-    Motor_Off();
+    motorOff();
     homingCompleted = true;
   }
 }
